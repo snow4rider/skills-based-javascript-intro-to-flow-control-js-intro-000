d0d0bdaddb7971c4cba67bb0c675cb0e0aea34c9
immutable-form-1096 <matthew.t.mckenney@gmail.com>
2021-01-15 06:13:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,11 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age <= 12){
+    return "You are a kid";
+  } else if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
 }
 
 function ternaryTeenager(age) {
